2b0cd33b8b46f2095a81ba112b77038951c97e42
Syrotenko <syrotenko.sergey.g@gmail.com>
2020-11-23 22:24:21 +0200

    Add main project

---
+++ WpfHelper/EnumBooleanConverter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace WpfHelper
+{
+    public class EnumBooleanConverter : IValueConverter
+    {
+        public object Convert (object value, Type targetType, object param, CultureInfo culture)
+        {
+            return value.Equals(param);
+        }
+
+        public object ConvertBack (object value, Type targetType, object param, CultureInfo culture)
+        {
+            return (bool)value ? param : Binding.DoNothing;
+        }
+    }
+}
---
+++ WpfHelper/WpfCommand.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace GenerateBranchName
+{
+    public class WpfCommand : ICommand
+    {
+        public WpfCommand(Action<object> action, Predicate<object> canExecuteAction) 
+        {
+            CustomAction = action;
+            CanExecuteAction = canExecuteAction;
+        }
+
+        readonly Action<object> CustomAction;
+        readonly Predicate<object> CanExecuteAction;
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return CanExecuteAction != null && CanExecuteAction.Invoke(parameter);
+        }
+
+        public void Execute(object parameter)
+        {
+            CustomAction?.Invoke(parameter);
+        }
+    }
+}
